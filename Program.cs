ad9f6967c14423d3054ad8029ef3152074db8c5d
lenhat41555 <70925729+lenhat41555@users.noreply.github.com>
2020-09-10 23:41:03 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace tienlai
+{
+    class Program
+    {
+        static void tinh(){
+          
+            double money = 1.0;
+            int month = 1;
+            double intersetRate = 1.0;
+
+            Console.WriteLine("Enter investment amount: ");
+            money = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of months: ");
+            month = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter annual interest rate in percentage: ");
+            intersetRate = Double.Parse(Console.ReadLine());
+             double totalInterset = 0;
+            for(int i = 0; i < month; i++)
+            {
+                totalInterset = money * (intersetRate/100)/12 * 3; 
+            }
+            Console.WriteLine("Total of interset: " + totalInterset);
+        }
+        static void Main(string[] args)
+        {
+            tinh();
+        }
+    }
+}
+`
